de4986fe7b1e97ec6f42ab191995da6d28fc7300
kubakakauko <47721520+kubakakauko@users.noreply.github.com>
2023-08-10 00:27:17 +0100

    Removed redundant file

--- common/constants.js
+++ common/constants.js
@@ -6,8 +6,8 @@ constants.DATA_DATASET = constants.DATA_DIR + "/dataset";
 constants.JSON_DIR = constants.DATA_DATASET + "/json";
 constants.IMG_DIR = constants.DATA_DATASET + "/img";
 constants.SAMPLES = constants.DATA_DATASET + "/samples.json";
-constants.COMMON = "../common"
-constants.JS_OBJECTS = constants.COMMON + "/js_objects"; 
+constants.COMMON = "../common";
+constants.JS_OBJECTS = constants.COMMON + "/js_objects";
 constants.SAMPLES_JS = constants.JS_OBJECTS + "/samples.js";
 
 if (typeof module !== "undefined") module.exports = constants;
