d37c34181349d920e7de05af75f7f74c1a8b3a20
kubakakauko <47721520+kubakakauko@users.noreply.github.com>
2023-08-30 21:09:15 +0100

    features (path and point count)

--- node/dataset_generator.js
+++ node/dataset_generator.js
@@ -1,52 +1,74 @@
-const draw = require("../common/draw.js");
-const utils = require("../common/utils.js");
-const constants = require("../common/constants.js");
+const draw=require('../common/draw.js');
+const constants=require('../common/constants.js');
+const utils=require('../common/utils.js');
 
-const { createCanvas } = require("canvas");
-const canvas = createCanvas(400, 400);
-const ctx = canvas.getContext("2d");
+const {createCanvas}=require('canvas');
+const canvas=createCanvas(400,400);
+const ctx=canvas.getContext('2d');
 
-const fs = require("fs");
-const fileNames = fs.readdirSync(constants.DATA_RAW);
-const samples = [];
+const fs=require('fs');
 
-let id = 1;
+// This extra section helps to install the package (was not in the tutorial)
+if(fs.existsSync(constants.DATASET_DIR)){
+   fs.readdirSync(constants.DATASET_DIR).forEach(fileName => 
+      fs.rmSync(constants.DATASET_DIR+"/"+fileName,{recursive:true}
+   ));
+   fs.rmdirSync(constants.DATASET_DIR);
+}
+fs.mkdirSync(constants.DATASET_DIR);
+fs.mkdirSync(constants.JSON_DIR);
+fs.mkdirSync(constants.IMG_DIR);
+console.log("GENERATING DATASET ...");
+// End of extra section
 
-// going through each file, we get a file name (fn)
-fileNames.forEach((fn) => {
-  const content = fs.readFileSync(constants.DATA_RAW + "/" + fn);
-  const { session, student, drawings } = JSON.parse(content);
+const fileNames=fs.readdirSync(constants.RAW_DIR);
+const samples=[];
+let id=1;
+fileNames.forEach(fn=>{
+   const content=fs.readFileSync(
+      constants.RAW_DIR+"/"+fn
+   );
+   const {session,student,drawings}=JSON.parse(content);
+   for(let label in drawings){
+      samples.push({
+         id,
+         label,
+         student_name:student,
+         student_id:session
+      });
 
-  for (let label in drawings) {
-    samples.push({
-      id,
-      label,
-      sudent_name: student,
-      student_id: session,
-    });
+      const paths=drawings[label];
+      fs.writeFileSync(
+         constants.JSON_DIR+"/"+id+".json",
+         JSON.stringify(paths)
+      );
 
-    const paths = drawings[label];
+      generateImageFile(
+         constants.IMG_DIR+"/"+id+".png",
+         paths
+      );
 
-    fs.writeFileSync(
-      constants.JSON_DIR + "/" + id + ".json",
-      JSON.stringify(paths),
-    );
-
-    generateImageFile(constants.IMG_DIR + "/" + id + ".png", paths);
-    utils.printProgress(id, fileNames.length * 8);
-    id++;
-  }
+      utils.printProgress(id,fileNames.length*8);
+      id++;
+   }
 });
 
-fs.writeFileSync(constants.SAMPLES, JSON.stringify(samples));
-fs.writeFileSync(
-  constants.SAMPLES_JS,
-  "const samples=" + JSON.stringify(samples) + ";",
+fs.writeFileSync(constants.SAMPLES,
+   JSON.stringify(samples)
 );
 
-function generateImageFile(outFile, paths) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  draw.paths(ctx, paths);
-  const buffer = canvas.toBuffer("image/png");
-  fs.writeFileSync(outFile, buffer);
+fs.mkdirSync(constants.JS_OBJECTS, {recursive: true});
+fs.writeFileSync(constants.SAMPLES_JS,
+   "const samples="+JSON.stringify(samples)+";"
+);
+
+function generateImageFile(outFile,paths){
+   ctx.clearRect(0,0,
+      canvas.width,canvas.height
+   );
+
+   draw.paths(ctx,paths);
+
+   const buffer=canvas.toBuffer("image/png");
+   fs.writeFileSync(outFile,buffer);
 }
