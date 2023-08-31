a48539af1473691b47295e7e99d31a839670f1b9
kubakakauko <47721520+kubakakauko@users.noreply.github.com>
2023-08-31 01:27:49 +0100

    Feature Extractor

---
+++ node/feature_extractor.js
@@ -0,0 +1,22 @@
+const constants = require("../common/constants.js");
+const features = require("../common/features.js");
+
+const fs = require("fs");
+
+const samples = JSON.parse(fs.readFileSync(constants.SAMPLES, "utf8"));
+
+for (const sample of samples) {
+  const paths = JSON.parse(
+    fs.readFileSync(constants.JSON_DIR + "/" + sample.id + ".json"),
+  );
+
+  sample.points = [featrues.getPathCount(paths), features.getPointCount(paths)];
+}
+
+const featureNames = ["Path Counts", "Point Counts"];
+
+fs.writeFileSync(
+  constants.FEATURES,
+  JSON.stringify({ featurenames: featureNames, samples: samples }, null, 2),
+  "utf8",
+);
